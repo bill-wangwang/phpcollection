9cc57b719eab511c5bdef38776d8946cda9c88b4
phpfull <285303886@qq.com>
2018-09-26 15:12:46 +0800

    tmall finish

--- application/common/service/collection/TmallService.php
+++ application/common/service/collection/TmallService.php
@@ -23,34 +23,53 @@ class TmallService {
     private function getTitle() {
         $pat = '/<h1 data-spm="1000983">(.*)<\/h1>/isU';
         preg_match($pat, $this->content, $match);
-        return  trim($match[1]);
+        return trim($match[1]);
     }
 
     private function getContent($url) {
-        $temp =  iconv('gbk', 'utf-8',file_get_contents($url));
+        $temp = iconv('gbk', 'utf-8', file_get_contents($url));
         return $temp;
     }
 
     private function getMainPic() {
         $pat = '/<img id="J_ImgBooth" alt="(.*)" src="(.*)"(.*)\/>/isU';
         preg_match($pat, $this->content, $match);
-        return  trim($match[2]);
+        $thumb_url = trim($match[2]);
+        $url = $this->getRealUrl($thumb_url);
+        return ['thumb_url' => $thumb_url, 'url' => $url];
     }
 
     private function getPicList() {
         $pat = '/<li class="(.*)">(.*)<a href="#"><img src="(.*)" \/><\/a>(.*)<\/li>/isU';
         preg_match_all($pat, $this->content, $match);
-        return   $match[3] ;
+        $thumb_urls = $match[3];
+        $urls = [];
+        foreach($thumb_urls as $url){
+            $urls[] = $this->getRealUrl($url);
+        }
+        return [
+            'thumb_urls' => $thumb_urls,
+            'urls'       => $urls
+        ];
     }
 
     private function getDetail() {
         $pat = '/"descUrl":"(.*)",/isU';
         preg_match($pat, $this->content, $match);
-        $descUrl = "http:"  . trim($match[1]);
-        $apiContent = iconv('gbk', 'utf-8',file_get_contents($descUrl));
+        $descUrl = "http:" . trim($match[1]);
+        $apiContent = iconv('gbk', 'utf-8', file_get_contents($descUrl));
         $patApiContent = "/var desc='(.*)';/isU";
         preg_match($patApiContent, $apiContent, $matchApiContent);
         return $matchApiContent[1];
+    }
+
+    private function getRealUrl($url) {
+        if (stripos($url, '//') === 0) {
+            $url = 'http:' . $url;
+        }
+        $temp = explode('.jpg_', $url);
+        $new_url = $temp[0] . '.jpg';
+        return $new_url;
     }
 
     public function getInfo() {
